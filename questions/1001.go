1724bcabcd47b4aaea6fbc082664a8a30e500e9a
范家豪 <jhfan@che300.com>
2020-02-14 12:20:50 +0800

    1001

--- questions/1001.go
+++ questions/1001.go
@@ -42,10 +42,10 @@ leetcode地址 : https://leetcode-cn.com/problems/grid-illumination/
 提示：
 
 
-	1 <= N <= 10^9
-	0 <= lamps.length <= 20000
-	0 <= queries.length <= 20000
-	lamps[i].length == queries[i].length == 2
+    1 <= N <= 10^9
+    0 <= lamps.length <= 20000
+    0 <= queries.length <= 20000
+    lamps[i].length == queries[i].length == 2
 
 
  */
@@ -53,12 +53,90 @@ package main
 
 import(
     "fmt"
+    "math"
 )
 
 func main(){
-    fmt.Println("请完成你的逻辑代码")
+    n := 5
+    lamps := [][]int {
+        {0, 0},
+        {4, 4},
+    }
+    queries := [][]int {
+        {1, 1},
+        {1, 0},
+    }
+
+    res := gridIllumination(n, lamps, queries)
+    fmt.Println(res)
 }
 
 func gridIllumination(N int, lamps [][]int, queries [][]int) []int {
-    
+    res := []int {}
+
+    for i := 0; i < len(queries); i++ {
+        // 生成照亮区域
+        area := generateLightArea(N, lamps)
+
+        // 返回指定区域是否被照亮
+        x := queries[i][0]
+        y := queries[i][1]
+        res = append(res, area[x][y])
+
+        // 关闭周围灯光
+        lamps = turnDownLamp(queries[i], lamps)
+    }
+
+    return res
+}
+
+func generateLightArea(N int, lamps [][]int) [][]int {
+    area := make([][]int, N)
+    for i := 0; i < N; i++ {
+        area[i] = make([]int, N)
+    }
+
+    for l := 0; l < len(lamps); l++ {
+        x := lamps[l][0]
+        y := lamps[l][1]
+
+        for i := 0; i < N; i++ {
+            for j := 0; j < N; j++ {
+                if i == x {
+                    area[i][j] = 1
+                }
+
+                if j == y {
+                    area[i][j] = 1
+                }
+
+                if i - x == j - y {
+                    area[i][j] = 1
+                }
+
+                if i - x == y - j {
+                    area[i][j] = 1
+                }
+            }
+        }
+    }
+
+    return area
+}
+
+func turnDownLamp(point []int, lamps [][]int) [][]int {
+    for i := 0; i < len(lamps); i++ {
+        x := point[0]
+        y := point[1]
+
+        if math.Abs(float64(x - lamps[i][0])) <= 1 && math.Abs(float64(y - lamps[i][1])) <= 1 {
+            if i + 1 < len(lamps) {
+                lamps = append(lamps[:i], lamps[i + 1:]...)
+            } else {
+                lamps = lamps[:i]
+            }
+        }
+    }
+
+    return lamps
 }
