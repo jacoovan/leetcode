eeff2d0a4d0d7cb8bedcbe1cbbabf42c88d0397b
范家豪 <jhfan@che300.com>
2020-03-14 15:17:57 +0800

    42

--- questions/42.go
+++ questions/42.go
@@ -23,9 +23,119 @@ import(
 )
 
 func main(){
-    fmt.Println("请完成你的逻辑代码")
+    height := []int {0,1,0,2,1,0,1,3,2,1,2,1}
+
+    res := trap(height)
+
+    fmt.Println(res)
 }
 
 func trap(height []int) int {
-    
+    barrels := getBarrels(height)
+
+    volume := 0
+    for _, barrel := range barrels {
+        volume += getVolume(barrel, height)
+    }
+
+    return volume
+}
+
+func getBarrels(height []int) [][]int {
+    if len(height) == 0 {
+        return [][]int {}
+    }
+
+    first := getFirstMax(height)
+    second := 0
+    if first + 1 < len(height) {
+        second = getSecondMax(height[first:])
+        if second == 0 {
+            return [][]int {}
+        }
+        second = first + second
+    } else {
+        return [][]int {}
+    }
+
+    barrels := [][]int {
+        {first, second},
+    }
+    if second + 1 < len(height) {
+
+        nextBarrels := getBarrels(height[second:])
+
+        if len(nextBarrels) > 0 {
+            for _, barrel := range nextBarrels {
+                barrel[0] += second
+                barrel[1] += second
+                barrels = append(barrels, barrel)
+            }
+        }
+
+        return barrels
+    } else {
+        return [][]int {}
+    }
+}
+
+func getFirstMax(height []int) int {
+    maxIndex := 0
+
+    for i := 1; i < len(height); i++ {
+        if height[i] < height[maxIndex] {
+            break
+        } else {
+            maxIndex = i
+        }
+    }
+
+    return maxIndex
+}
+
+func getSecondMax(height []int) int {
+    maxIndex := 0
+
+    sub := true
+    for i := 1; i < len(height); i++ {
+        if sub && height[i] <= height[i - 1] {
+            continue
+        }
+
+        if sub && height[i] > height[i - 1] {
+            sub = false
+            continue
+        }
+
+        if !sub && height[i - 1] >= height[i] {
+            maxIndex = i - 1
+            break
+        }
+
+        // 未完待续...
+    }
+
+    return maxIndex
+}
+
+func getVolume(barrel []int, height []int) int {
+    start := barrel[0]
+    end   := barrel[1]
+
+    maxHeight := 0
+    if height[start] > height[end] {
+        maxHeight = height[end]
+    } else {
+        maxHeight = height[start]
+    }
+
+    volumn := 0
+    for i := start; i <= end; i++ {
+        if height[i] > maxHeight {
+            continue
+        }
+        volumn += maxHeight - height[i]
+    }
+
+    return volumn
 }
