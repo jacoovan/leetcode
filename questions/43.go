091b5eddf84e02b8a0c2579a9a164a536d6126fc
范家豪 <jhfan@che300.com>
2020-03-15 11:09:01 +0800

    43

--- questions/43.go
+++ questions/43.go
@@ -28,14 +28,90 @@ leetcode地址 : https://leetcode-cn.com/problems/multiply-strings/
  */
 package main
 
-import(
+import (
     "fmt"
+    "math"
+    "strconv"
+    "strings"
 )
 
 func main(){
-    fmt.Println("请完成你的逻辑代码")
+    num1 := "123"
+    num2 := "456"
+    res := multiply(num1, num2)
+
+    fmt.Println(res)
 }
 
 func multiply(num1 string, num2 string) string {
-    
+    nums1 := strings.Split(num1, "")
+    nums2 := strings.Split(num2, "")
+
+    tempResult := map[int]int {}
+    pos := 0
+    for i, first := range nums1 {
+        for j, second := range nums2 {
+            n1, _ := strconv.Atoi(first)
+            n2, _ := strconv.Atoi(second)
+
+
+            r := n1 * n2
+            r0 := r - int(math.Floor(float64(r) / 10.0)) * 10
+            r1 := int(math.Floor(float64(r) / 10.0))
+
+            pos = i + j
+
+            v, ok := tempResult[pos]
+            if !ok {
+                tempResult[pos] = r0
+            } else if v + r0 < 10 {
+                tempResult[pos] += r0
+            } else {
+                tempResult[pos] += r0
+                tempResult[pos] -= 10
+                r1 += 1
+            }
+
+            if r1 > 0 {
+                v1, _ := tempResult[pos - 1]
+                if v1 + r1 < 10 {
+                    tempResult[pos - 1] += r1
+                } else {
+                    tempResult[pos - 1] += r1
+                    tempResult[pos - 1] -= 10
+
+                    for {
+                        pos  = pos - 1
+                        v2, ok := tempResult[pos - 1]
+                        if !ok {
+                            v2 = 1
+                            tempResult[pos - 1] = v2
+                            break
+                        } else if v2 + 1 < 10 {
+                            v2 += 1
+                            tempResult[pos - 1] = v2
+                            break
+                        } else {
+                            v2 = 0
+                            tempResult[pos - 1] = 0
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+    min := 0
+    for i, _ := range tempResult {
+       if i < min {
+           min = i
+       }
+    }
+    res := ""
+    for i := min; i < min + len(tempResult); i++ {
+        res += strconv.Itoa(tempResult[i])
+    }
+
+    return res
 }
