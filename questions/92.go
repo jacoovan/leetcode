7b42a0ec9db906ef89ed1d4a6e244cb70102f5f8
morgen <mmmorgen@163.com>
2020-09-20 10:56:14 +0800

    92

--- questions/92.go
+++ questions/92.go
@@ -23,6 +23,23 @@ import(
 
 func main(){
     fmt.Println("请完成你的逻辑代码")
+    list := &ListNode{
+        Val:  1,
+        Next: nil,
+    }
+    list.Next = &ListNode{Val: 2, Next: nil}
+    list.Next.Next = &ListNode{Val: 3, Next: nil}
+    list.Next.Next.Next = &ListNode{Val: 4, Next: nil}
+    list.Next.Next.Next.Next = &ListNode{Val: 5, Next: nil}
+
+    result := reverseBetween(list, 1, 4)
+    _ = result
+    temp := result
+    for temp.Next != nil {
+      fmt.Println("val:", temp.Val)
+      temp = temp.Next
+    }
+    fmt.Println("val:", temp.Val)
 }
 
 /**
@@ -32,6 +49,46 @@ func main(){
  *     Next *ListNode
  * }
  */
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
 func reverseBetween(head *ListNode, m int, n int) *ListNode {
-    
+    var i = 1
+    nextNode := head.Next
+
+    prevNode := head
+    if m == 1 {
+        prevNode = nil
+    }
+    endNode := &ListNode{Val: head.Val, Next: head.Next}
+    for i < m {
+        if i > 1 {
+            prevNode = prevNode.Next
+        }
+        endNode = nextNode
+        nextNode = nextNode.Next
+        i++
+    }
+
+    startNode := endNode
+    for i >= m && i < n {
+        currentNode := nextNode
+        nextNode = nextNode.Next
+        tempNode := startNode
+        startNode = currentNode
+        startNode.Next = tempNode
+        i++
+    }
+    afterNode := nextNode
+    endNode.Next = afterNode
+
+    if prevNode == nil {
+        return startNode
+    }
+    prevNode.Next = startNode
+    endNode.Next = afterNode
+    return head
 }
