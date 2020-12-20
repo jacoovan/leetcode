f375b957aa96afc409d18ee2368df31ed5117e2b
morgen <jacoovan@jacoovan.local>
2020-12-20 08:33:39 +0800

    306

--- questions/306.go
+++ questions/306.go
@@ -15,30 +15,84 @@ leetcode地址 : https://leetcode-cn.com/problems/additive-number/
 示例 1:
 
 输入: "112358"
-输出: true 
+输出: true
 解释: 累加序列为: 1, 1, 2, 3, 5, 8 。1 + 1 = 2, 1 + 2 = 3, 2 + 3 = 5, 3 + 5 = 8
 
 
 示例 2:
 
 输入: "199100199"
-输出: true 
+输出: true
 解释: 累加序列为: 1, 99, 100, 199。1 + 99 = 100, 99 + 100 = 199
 
 进阶:<br>
 你如何处理一个溢出的过大的整数输入?
 
- */
+*/
 package main
 
-import(
-    "fmt"
+import (
+	"fmt"
+	"strconv"
 )
 
-func main(){
-    fmt.Println("请完成你的逻辑代码")
+func main() {
+	str := "1124"
+	res := isAdditiveNumber(str)
+	fmt.Println("res:", res)
+}
+
+type num struct {
+	StartIndex int
+	EndIndex   int
+	Value      []byte
+}
+
+func newNum(startIndex int) *num {
+	return &num{
+		StartIndex: startIndex,
+		EndIndex:   startIndex - 1,
+		Value:      make([]byte, 0),
+	}
+}
+
+func (n *num) Add(char byte) *num {
+	n.EndIndex = n.EndIndex + 1
+	n.Value = append(n.Value, char)
+	return n
+}
+
+func (n *num) GenerateInt() int {
+	n1, _ := strconv.Atoi(string(n.Value))
+	return n1
 }
 
 func isAdditiveNumber(num string) bool {
-    
+	total := len(num)
+	var i, j, k int
+	var n1, n2, n3 int
+	var exist = false
+	temp1 := newNum(i)
+	if total-2 == 0 {
+		return true
+	}
+	for i = 0; i < total-2; i++ {
+		temp1.Add(num[i])
+		n1 = temp1.GenerateInt()
+		temp2 := newNum(i + 1)
+		for j = i + 1; j < total-1; j++ {
+			temp2.Add(num[j])
+			n2 = temp2.GenerateInt()
+			temp3 := newNum(j + 1)
+			for k = j + 1; k < total; k++ {
+				temp3.Add(num[k])
+				n3 = temp3.GenerateInt()
+				if n3 == n1+n2 {
+					exist = isAdditiveNumber(num[i+1:])
+					return exist
+				}
+			}
+		}
+	}
+	return exist
 }
