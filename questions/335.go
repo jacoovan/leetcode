9c7d266a664e98fc005438678b44e36135c0e1f3
morgen <jacoovan@jacoovan.local>
2020-12-27 11:44:09 +0800

    335

--- questions/335.go
+++ questions/335.go
@@ -8,7 +8,7 @@ leetcode地址 : https://leetcode-cn.com/problems/self-crossing/
 
 编写一个 O(1) 空间复杂度的一趟扫描算法，判断你所经过的路径是否相交。
 
- 
+
 
 示例 1:
 
@@ -18,7 +18,7 @@ leetcode地址 : https://leetcode-cn.com/problems/self-crossing/
     │
 
 输入: [2,1,1,2]
-输出: true 
+输出: true
 
 
 示例 2:
@@ -30,7 +30,7 @@ leetcode地址 : https://leetcode-cn.com/problems/self-crossing/
 └────────────>
 
 输入: [1,2,3,4]
-输出: false 
+输出: false
 
 
 示例 3:
@@ -40,20 +40,107 @@ leetcode地址 : https://leetcode-cn.com/problems/self-crossing/
 └───┼>
 
 输入: [1,1,1,1]
-输出: true 
+输出: true
 
 
- */
+*/
 package main
 
-import(
-    "fmt"
+import (
+	"fmt"
 )
 
-func main(){
-    fmt.Println("请完成你的逻辑代码")
+func main() {
+	x := []int{1, 2, 3, 4, 1, 4}
+	res := isSelfCrossing(x)
+	fmt.Println("res:", res)
 }
 
 func isSelfCrossing(x []int) bool {
-    
+	if len(x) < 4 {
+		return false
+	}
+
+	if x[0] >= x[2] && x[1] <= x[3] {
+		return true
+	}
+
+	if len(x) == 4 {
+		return false
+	}
+
+	lastTop := x[0]
+	lastLeft := -x[1]
+	lastBottom := x[0] - x[2]
+	lastRight := x[3] - x[1]
+
+	inner := x[0] > x[2] || x[1] > x[3]
+	x = x[4:]
+	for i := range x {
+		if inner {
+			if i%4 == 0 && lastBottom+x[i] >= lastTop {
+				lastTop = lastBottom + x[i]
+				return true
+			}
+			if i%4 == 1 && lastRight-x[i] <= lastLeft {
+				lastLeft = lastRight - x[i]
+				return true
+			}
+			if i%4 == 2 && lastTop-x[i] <= lastBottom {
+				lastBottom = lastTop - x[i]
+				return true
+			}
+			if i%4 == 3 && lastLeft+x[i] >= lastRight {
+				lastRight = lastLeft + x[i]
+				return true
+			}
+			continue
+		}
+		if i%4 == 0 && lastBottom+x[i] < lastTop {
+			lastTop = lastBottom + x[i]
+			if i >= 4 {
+				lastRight = lastLeft + (x[i-1] - x[i-3])
+				if x[i] >= x[i-2]-x[i-4] && x[i] <= x[i-2] {
+					lastLeft = lastRight
+				}
+			}
+			inner = true
+			continue
+		}
+
+		if i%4 == 1 && lastRight-x[i] > lastLeft {
+			lastLeft = lastRight - x[i]
+			if i >= 4 {
+				lastTop = lastBottom + (x[i-1] - x[i-3])
+				if x[i] >= x[i-2]-x[i-4] && x[i] <= x[i-2] {
+					lastBottom = lastTop
+				}
+			}
+			inner = true
+			continue
+		}
+		if i%4 == 2 && lastTop-x[i] > lastBottom {
+			lastBottom = lastTop - x[i]
+			if i >= 4 {
+				lastLeft = lastRight - (x[i-1] - x[i-3])
+				if x[i] >= x[i-2]-x[i-4] && x[i] <= x[i-2] {
+					lastRight = lastLeft
+				}
+			}
+			inner = true
+			continue
+		}
+		if i%4 == 3 && lastLeft+x[i] < lastRight {
+			lastRight = lastLeft + x[i]
+			if i >= 4 {
+				lastBottom = lastTop - (x[i-1] - x[i-3])
+				if x[i] >= x[i-2]-x[i-4] && x[i] <= x[i-2] {
+					lastTop = lastBottom
+				}
+			}
+			inner = true
+			continue
+		}
+	}
+	return false
 }
