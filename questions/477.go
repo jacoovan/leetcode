3df5c29a9309acb9cb1a5c86a1c753db5f84e50e
jacoovan <jacoovan@163.com>
2021-01-31 08:58:51 +0800

    477

--- questions/477.go
+++ questions/477.go
@@ -27,17 +27,75 @@ HammingDistance(4, 14) + HammingDistance(4, 2) + HammingDistance(14, 2) = 2 + 2 
 	数组的长度不超过 10^4。
 
 
- */
+*/
 package main
 
-import(
-    "fmt"
+import (
+	"fmt"
 )
 
-func main(){
-    fmt.Println("请完成你的逻辑代码")
+func main() {
+	nums := []int{4, 14, 2}
+	res := totalHammingDistance(nums)
+	fmt.Println("res:", res)
 }
 
 func totalHammingDistance(nums []int) int {
-    
+	length := len(nums) - 1
+	distance := 0
+	for i := range nums[:length] {
+		for j := i + 1; j <= length; j++ {
+			distance = distance + getHammingDistance(nums[i], nums[j])
+		}
+	}
+	return distance
+}
+
+func getHammingDistance(n1, n2 int) int {
+	biggerCode := make([]int, 0)
+	smallerCode := make([]int, 0)
+	if n1 > n2 {
+		biggerCode = getHammingCode(n1)
+		smallerCode = getHammingCode(n2)
+	} else {
+		biggerCode = getHammingCode(n2)
+		smallerCode = getHammingCode(n1)
+	}
+
+	fmt.Println(biggerCode, smallerCode)
+
+	distance := 0
+	smallerLength := len(smallerCode)
+	for i := range biggerCode {
+		if i < smallerLength {
+			if smallerCode[i] != biggerCode[i] {
+				distance = distance + 1
+			}
+			continue
+		}
+		if biggerCode[i] == 1 {
+			distance = distance + 1
+		}
+	}
+	return distance
+}
+
+func getHammingCode(num int) []int {
+	res := make([]int, 0)
+
+	if num%2 == 1 {
+		res = append(res, 1)
+		num = num - 1
+		num = num / 2
+		res = append(res, getHammingCode(num)...)
+		return res
+	}
+
+	if num > 0 && num%2 == 0 {
+		res = append(res, 0)
+		num = num / 2
+		res = append(res, getHammingCode(num)...)
+		return res
+	}
+	return res
 }
